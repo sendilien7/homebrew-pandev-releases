7c324ffd638c5d69c6e02b92070a2ac7bab7ad2e
sendilien7 <sendilien23@gmail.com>
2026-01-23 16:17:32 +0500

    Fix

--- Formula/pandev-cli-plugin.rb
+++ Formula/pandev-cli-plugin.rb
@@ -1,15 +1,15 @@
 # Formula/pandev-cli-plugin.rb
 class PandevCliPlugin < Formula
   desc "Your CLI Plugin Description"
-  homepage "https://github.com/sendilien7/pandev-releases"
+  homepage "https://github.com/sendilien7/homebrew-pandev-releases"
   version "1.0.0" # This must match the version in your build workflow
 
   if Hardware::CPU.intel?
-    url "https://github.com/sendilien7/pandev-releases/releases/download/v1.0.0/pandev-cli-plugin_1.0.0_macOS_amd64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Replace this after your first release
+    url "https://github.com/sendilien7/homebrew-pandev-releases/releases/download/v1.0.0/pandev-cli-plugin_1.0.0_macOS_amd64.tar.gz"
+    sha256 "your_amd64_hash"
   else
-    url "https://github.com/sendilien7/pandev-releases/releases/download/v1.0.0/pandev-cli-plugin_1.0.0_macOS_arm64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Replace this after your first release
+    url "https://github.com/sendilien7/homebrew-pandev-releases/releases/download/v1.0.0/pandev-cli-plugin_1.0.0_macOS_arm64.tar.gz"
+    sha256 "your_arm64_hash"
   end
 
   def install
